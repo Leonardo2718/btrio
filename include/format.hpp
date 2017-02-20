e330f53fcc212c333f27ff72f6a08226e3c51822
Leonardo Banderali <leonardo2718@protonmail.com>
2017-02-20 13:59:40 -0500

    Add support for padding a side

--- include/format.hpp
+++ include/format.hpp
@@ -8,12 +8,40 @@
 #ifndef FORMAT_HPP
 #define FORMAT_HPP
 
-#define FORMAT_ARGS _radix, _decimals, _minw, _maxw, _fill
-#define FROMAT_TYPES unsigned int, unsigned int, unsigned int, unsigned int, char
-#define FORMAT_TARGS unsigned int _radix, unsigned int _decimals, \
-    unsigned int _minw, unsigned int _maxw, char _fill
-#define FORMAT_DEFAULT_TARGS unsigned int _radix = 10, unsigned int _decimals = 3, \
-    unsigned int _minw = 0, unsigned int _maxw = -1, char _fill = ' '
+namespace btrio {
+
+enum class Side {Left, Right};
+
+}
+
+#define FORMAT_ARGS \
+    _radix, \
+    _decimals, \
+    _pad_side, \
+    _minw, \
+    _maxw, \
+    _fill
+#define FROMAT_TYPES \
+    unsigned int, \
+    unsigned int, \
+    btrio::Side, \
+    unsigned int, \
+    unsigned int, \
+    char
+#define FORMAT_TARGS \
+    unsigned int _radix, \
+    unsigned int _decimals, \
+    btrio::Side _pad_side, \
+    unsigned int _minw, \
+    unsigned int _maxw, \
+    char _fill
+#define FORMAT_DEFAULT_TARGS \
+    unsigned int _radix = 10, \
+    unsigned int _decimals = 3, \
+    btrio::Side _pad_side = btrio::Side::Left, \
+    unsigned int _minw = 0, \
+    unsigned int _maxw = -1, \
+    char _fill = ' '
 
 #define FORMAT_TEMPLATE template <FORMAT_TARGS>
 #define FORMAT_DEFAULT_TEMPLATE template<FORMAT_DEFAULT_TARGS>
@@ -38,27 +66,31 @@ struct btrio::static_format {
 
         static constexpr auto get_radix() { return _radix; }
         static constexpr auto get_decimals() { return _decimals; }
+        static constexpr auto get_pad_side() { return _pad_side; }
         static constexpr auto get_minw() { return _minw; }
         static constexpr auto get_maxw() { return _maxw; }
         static constexpr auto get_fill() { return _fill; }
 
         template <unsigned int r>
-        using radix = static_format<r, _decimals, _minw, _maxw, _fill>;
+        using radix = static_format<r, _decimals, _pad_side, _minw, _maxw, _fill>;
 
         template <unsigned int d>
-        using decimals = static_format<_radix, d, _minw, _maxw, _fill>;
+        using decimals = static_format<_radix, d, _pad_side, _minw, _maxw, _fill>;
+
+        template <btrio::Side s>
+        using pad_side = static_format<_radix, _decimals, s, _minw, _maxw, _fill>;
 
         template <unsigned int w>
-        using minw = static_format<_radix, _decimals, w, _maxw, _fill>;
+        using minw = static_format<_radix, _decimals, _pad_side, w, _maxw, _fill>;
 
         template <unsigned int w>
-        using maxw = static_format<_radix, _decimals, _minw, w, _fill>;
+        using maxw = static_format<_radix, _decimals, _pad_side, _minw, w, _fill>;
 
         template <unsigned int w>
-        using setw = static_format<_radix, _decimals, w, w, _fill>;
+        using setw = static_format<_radix, _decimals, _pad_side, w, w, _fill>;
 
         template <char c>
-        using fill = static_format<_radix, _decimals, _minw, _maxw, c>;
+        using fill = static_format<_radix, _decimals, _pad_side, _minw, _maxw, c>;
 
         using _this = static_format<FORMAT_ARGS>;
 
