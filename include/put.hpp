2d43ed189f906aba1b44b25a2c168be03997cf8b
Leonardo Banderali <leonardo2718@protonmail.com>
2017-02-25 15:51:12 -0500

    Add more generic implementations of put

--- include/put.hpp
+++ include/put.hpp
@@ -33,48 +33,50 @@ static const char char_map[btrio::default_format::max_radix] = {
     'f'
 };
 
-template <typename T> void put(T arg);
+template <unsigned int radix, typename OutputIterator, typename T>
+OutputIterator itoa_lsdfirst(OutputIterator begin, OutputIterator end, T arg) {
+    static_assert( btrio::static_format::min_radix <= radix && radix <= btrio::static_format::max_radix,
+                   "Radix is outside the supported range [2,16].");
+    static_assert( std::is_integral<T>::value,
+                   "Can only cal"
+                   "l btrio::itoa_lsdfirst on an integral type.");
 
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value && std::is_signed<T>::value>::type put(T arg, FILE* f) {
-    char buf[20];
+    auto val = arg;
+    auto cursor = begin;
 
-    bool neg = arg < 0;
-    if (neg) {
-        std::putc('-', f);
-        arg = -arg;
+    while (cursor != end) {
+        auto d = val % radix;
+        *cursor = char_map[d];
+        val /= radix;
+        ++cursor;
+        if (val == 0) break;
     }
 
-    int i = 0;
-    do {
-        auto d = arg % 10;
-        buf[i] = '0' + d;
-        arg /= 10;
-        ++i;
-    } while (arg && i < sizeof(buf));
-
-    do {
-        --i;
-        std::putc(buf[i], f);
-    } while (i);
+    return cursor;
 }
 
-template <typename F, typename T>
+template <typename T> void put(T arg);
+
+template <typename OutputIterator, typename F, typename T>
 typename std::enable_if<
     std::is_integral<T>::value &&
     std::is_signed<T>::value &&
-    F::get_pad_side() == btrio::Side::Right >
-::type put(btrio::formatted_value<F, T> arg, FILE* f) {
+    F::get_pad_side() == btrio::Side::Right,
+    OutputIterator>
+::type iput(OutputIterator begin, OutputIterator end, btrio::formatted_value<F, T> arg) {
     constexpr auto radix = F::get_radix();
     auto minw = F::get_minw();
     auto maxw = F::get_maxw();
     char buf[80];
     auto val = arg.value;
+    auto cursor = begin;
 
     // print `-` if value is negative
     if (val < 0 && maxw > 0) {
-        std::putc('-', f);
         val = -val;
+        *cursor = '-';
+        ++cursor;
+        if (cursor == end) return cursor;
         if (minw != 0) --minw;
         --maxw;
     }
@@ -91,7 +93,9 @@ typename std::enable_if<
 
     // print leading zeros for padding
     for (auto count = i; count <= F::get_padding_zeros() && maxw > 0; ++count) {
-        std::putc('0', f);
+        *cursor = '0';
+        ++cursor;
+        if (cursor == end) return cursor;
         if (minw != 0) --minw;
         --maxw;
     }
@@ -99,7 +103,9 @@ typename std::enable_if<
     // print buffered  value
     while (i > 0 && maxw > 0) {
         --i;
-        std::putc(buf[i], f);
+        *cursor = buf[i];
+        ++cursor;
+        if (cursor == end) return cursor;
         if (minw != 0) --minw;
         --maxw;
     }
@@ -107,16 +113,135 @@ typename std::enable_if<
     // print padding
     for (minw; minw > 0; --minw) {
         // note that minw <= maxw must be true
-        std::putc(F::get_fill(), f);
+        *cursor = F::get_fill();
+        ++cursor;
+        if (cursor == end) return cursor;
+    }
+
+    return cursor;
+}
+
+template <typename OutputIterator, typename F, typename T>
+typename std::enable_if<
+    std::is_integral<T>::value &&
+    std::is_signed<T>::value &&
+    F::get_pad_side() == btrio::Side::Left,
+    OutputIterator>
+::type iput(OutputIterator begin, OutputIterator end, btrio::formatted_value<F, T> arg) {
+    constexpr auto radix = F::get_radix();
+    auto minw = F::get_minw();
+    auto maxw = F::get_maxw();
+    char buf[80];
+    auto val = arg.value;
+    auto cursor = begin;
+
+    // save whether value is negative
+    auto neg = val < 0;
+    if (neg) {
+        val = -val;
+    }
+
+    // buffer stringified value LSD first
+    int i = 0;
+    while (i < sizeof(buf)) {
+        auto d = val % radix;
+        buf[i] = char_map[d];
+        val /= radix;
+        ++i;
+        if (val == 0) break;
+    }
+
+    // buffer leading zeros for padding
+    for (i; i <= F::get_padding_zeros() && i < sizeof(buf); ++i) {
+        buf[i] = '0';
+    }
+
+    // buffer minus sign
+    if (neg) {
+        buf[i] = '-';
+        ++i;
+    }
+
+    // print padding
+    for (minw; minw > i; --minw) {
+        // note that minw <= maxw must be true
+        *cursor = F::get_fill();
+        ++cursor;
+        if (cursor == end) return cursor;
+        --maxw;
+    }
+
+    // print buffered value
+    while (i > 0 && maxw > 0) {
+        --i;
+        *cursor = buf[i];
+        ++cursor;
+        if (cursor == end) return cursor;
+        --maxw;
+    }
+
+    return cursor;
+}
+
+
+template <typename F, typename T, typename Putc, typename... PutcArgs>
+typename std::enable_if<
+    std::is_integral<T>::value &&
+    std::is_signed<T>::value &&
+    F::get_pad_side() == btrio::Side::Right >
+::type pput(btrio::formatted_value<F, T> arg, Putc putc, PutcArgs... putcArgs) {
+    constexpr auto radix = F::get_radix();
+    auto minw = F::get_minw();
+    auto maxw = F::get_maxw();
+    char buf[80];
+    auto val = arg.value;
+
+    // print `-` if value is negative
+    if (val < 0 && maxw > 0) {
+        val = -val;
+        putc('-', putcArgs...);
+        if (minw != 0) --minw;
+        --maxw;
+    }
+
+    // buffer stringified value LSD first
+    int i = 0;
+    while (i < sizeof(buf)) {
+        auto d = val % radix;
+        buf[i] = char_map[d];
+        val /= radix;
+        ++i;
+        if (val == 0) break;
+    }
+
+    // print leading zeros for padding
+    for (auto count = i; count <= F::get_padding_zeros() && maxw > 0; ++count) {
+        putc('0', putcArgs...);
+        if (minw != 0) --minw;
+        --maxw;
+    }
+
+    // print buffered  value
+    while (i > 0 && maxw > 0) {
+        --i;
+        putc(buf[i], putcArgs...);
+        if (minw != 0) --minw;
+        --maxw;
+    }
+
+    // print padding
+    for (minw; minw > 0; --minw) {
+        // note that minw <= maxw must be true
+        putc(F::get_fill(), putcArgs...);
     }
 }
 
-template <typename F, typename T>
+template <typename Putc, typename F, typename T, typename... PutcArgs>
 typename std::enable_if<
     std::is_integral<T>::value &&
     std::is_signed<T>::value &&
     F::get_pad_side() == btrio::Side::Left >
-::type put(btrio::formatted_value<F, T> arg, FILE* f) {
+::type pput(btrio::formatted_value<F, T> arg, Putc putc, PutcArgs... putcArgs) {
     constexpr auto radix = F::get_radix();
     auto minw = F::get_minw();
     auto maxw = F::get_maxw();
@@ -153,16 +278,69 @@ typename std::enable_if<
     // print padding
     for (minw; minw > i; --minw) {
         // note that minw <= maxw must be true
-        std::putc(F::get_fill(), f);
+        putc(F::get_fill(), putcArgs...);
         --maxw;
     }
 
     // print buffered value
     while (i > 0 && maxw > 0) {
         --i;
-        std::putc(buf[i], f);
+        putc(buf[i], putcArgs...);
         --maxw;
     }
+}
+
+
+template <typename F, typename T>
+typename std::enable_if<
+    std::is_integral<T>::value &&
+    std::is_signed<T>::value &&
+    F::get_pad_side() == btrio::Side::Right >
+::type put(btrio::formatted_value<F, T> arg, FILE* f) {
+    char buf[80];
+    auto end = iput(std::begin(buf), std::end(buf), arg);
+
+    for (auto cursor = std::begin(buf); cursor != end; ++cursor) {
+        std::putc(*cursor, f);
+    }
+}
+
+template <typename F, typename T>
+typename std::enable_if<
+    std::is_integral<T>::value &&
+    std::is_signed<T>::value &&
+    F::get_pad_side() == btrio::Side::Left >
+::type put(btrio::formatted_value<F, T> arg, FILE* f) {
+    char buf[80];
+    auto end = iput(std::begin(buf), std::end(buf), arg);
+
+    for (auto cursor = std::begin(buf); cursor != end; ++cursor) {
+        std::putc(*cursor, f);
+    }
+}
+
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value && std::is_signed<T>::value>::type put(T arg, FILE* f) {
+    char buf[20];
+
+    bool neg = arg < 0;
+    if (neg) {
+        std::putc('-', f);
+        arg = -arg;
+    }
+
+    int i = 0;
+    do {
+        auto d = arg % 10;
+        buf[i] = '0' + d;
+        arg /= 10;
+        ++i;
+    } while (arg && i < sizeof(buf));
+
+    do {
+        --i;
+        std::putc(buf[i], f);
+    } while (i);
 }
 
 template <typename T>
